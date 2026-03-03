3c9936c5aababa3b623969cd1bcf3b1d6f60ef20
victor176S <105952113+victor176S@users.noreply.github.com>
2026-03-03 21:42:41 +0100

    03/03/26 commit 2

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Numerics;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerAttack : MonoBehaviour
 {
 
     private Animator animator;
+    [SerializeField] private GameObject ataque;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,10 +30,28 @@ public class PlayerAttack : MonoBehaviour
         
         if (Input.GetMouseButtonDown(0) == true)
         {
+            GameObject objeto = Instantiate(ataque, this.gameObject.transform.position + new UnityEngine.Vector3(0,5,0), this.gameObject.transform.rotation);
+
+            objeto.transform.localRotation = quaternion.Euler(objeto.transform.localRotation.x, objeto.transform.localRotation.y, objeto.transform.localRotation.z);
+
+            MoverAtaque(objeto);
+
             animator.SetTrigger("BasicAttack");
         }
 
         yield return null;
 
     }
+
+    public async Task<UniTask> MoverAtaque(GameObject objeto)
+    {   
+        while (true)
+        {
+            objeto.transform.localPosition += new UnityEngine.Vector3(0.1f,0,0);
+            
+            await UniTask.Delay(1);
+        }
+        
+    }
+
 }
