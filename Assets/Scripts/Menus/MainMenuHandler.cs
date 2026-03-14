789322c10f93ed2802ece33ec887703bd4fe1ca3
KR13 <amaalam16a3@gmail.com>
2026-03-14 14:47:59 +0100

    Modificado el menú principal

--- Assets/Scripts/Menus/MainMenuHandler.cs
+++ Assets/Scripts/Menus/MainMenuHandler.cs
@@ -6,11 +6,25 @@ public class MainMenuHandler : MonoBehaviour
     [Header("Menu Screens")]
     [SerializeField] private GameObject loadingScreen;
     [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject optionsMenu;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         mainMenu.SetActive(true);
         loadingScreen.SetActive(false);
+        optionsMenu.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        // Directiva de preprocesador
+        #if UNITY_EDITOR
+            // Si estamos en el editor de Unity, usamos el comando para detener el juego.
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            // Si estamos en un ejecutable (Build), cerramos la aplicación.
+            Application.Quit();
+        #endif
     }
 }
